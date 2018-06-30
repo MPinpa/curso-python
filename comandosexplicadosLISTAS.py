da60a231b111630e1bb02353560e5ff2ebefa598
Cassiano <cassiano15dantas@gmail.com>
2018-06-30 09:49:48 -0300

    teste

---
+++ comandosexplicadosLISTAS.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Listas Colas
+
+alfabeto = [] # lista
+
+range(65, 65 + 26):  # range (inicio, fim)
+
+alfabeto.append(x) # adiciona elemento na lista
+
+alfabeto.sort() # organiza os elementos da listas
+
+alfabeto.reverse() # organiza os elementos da lista ao contrario
+
+alfabeto.index(valorASerBuscado) # Encontra o elemento na lista.
+
+alfabeto.cont(ValorASerBuscado) # quantas vezes o elemento aparece na lista
+
+alfabeto.remove(elementoaserremovido) # Remove primeiro encontrado da lista
+
+alfabeto.pop(posicaodoelemento) # Remove o elemento daquela posicao
+
